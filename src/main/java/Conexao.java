c7e53fa1de59bd8facafe08a94c9876ada8d9a43
MatheusLuca <luca_matheus@live.com>
2026-08-26 20:47:14 -0300

    Feat: Criacao da classe Conexao, variaveis do driver e banco estabelecidas, metodo loadDriver e conection.

---
+++ src/main/java/Conexao.java
@@ -0,0 +1,34 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexao {
+
+    private static String driver= "oracle.jdbc.driver.OracleDriver";
+    private static String url = "jdbc:oracle:thin:@oracle.fiap.com.br:1521:orcl ";
+    private static String usuario = "rm572228";
+    private static String senha = "260497";
+
+    public static void loadDriver(){
+        try{
+            Class.forName(driver);
+              System.out.println("Driver carregado com sucesso!");
+        } catch (ClassNotFoundException e) {
+             System.err.println("Erro ao carregar o driver do banco" + e.getMessage());
+        }
+    }
+
+    public static Connection conectar(){
+        try{
+            Connection conn = DriverManager.getConnection(url, usuario, senha);
+            System.out.println("Conexao com o banco oracle estabelecida!");
+            return conn;
+        } catch (SQLException e) {
+            System.out.println("Erro ao conectar no banco verificque url, usuario e senha" + e.getMessage());
+        }
+        return null;
+    }
+
+
+
+}
